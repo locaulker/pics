93c7afbc5270b6ba2dc4899a8792301dd597a07c
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-16 18:56:32 -0500

    passing data from child to parent

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react"
 import SearchBar from "./SearchBar"
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "2rem" }}>
-      <SearchBar />
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term)
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "2rem" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    )
+  }
 }
 
 export default App
